a0cd25e5da576b2310079fb28949645fc41e2f8f
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2021-12-27 17:35:28 +0800

    springcloud alibaba 测试项目

---
+++ spring_cloud_alibaba/order/src/main/java/com/xyy/order/OrderApplication.java
@@ -0,0 +1,19 @@
+package com.xyy.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zyf
+ * @create: 2021-12-27 17:22
+ **/
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrderApplication.class, args);
+
+
+    }
+
+}
