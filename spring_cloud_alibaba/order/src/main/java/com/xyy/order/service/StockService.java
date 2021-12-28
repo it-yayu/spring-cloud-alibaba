92f4e1d44262a96c958c30f448933fe84672e483
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2021-12-28 18:10:32 +0800

    代码修改

--- spring_cloud_alibaba/order/src/main/java/com/xyy/order/service/StockService.java
+++ spring_cloud_alibaba/order/src/main/java/com/xyy/order/service/StockService.java
@@ -1,6 +1,7 @@
 package com.xyy.order.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @create: 2021-12-28 10:14
  **/
 @FeignClient(value = "stock-service",fallback = StockFallbackService.class)
+@Component
 public interface StockService {
     /**
      * 修改库存
